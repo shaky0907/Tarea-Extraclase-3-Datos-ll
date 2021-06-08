a73f10e3f957df9c416102ea0a13ea8b4456be58
shaky0907 <62965535+shaky0907@users.noreply.github.com>
2021-06-08 17:59:55 -0600

    First commit

---
+++ TareaEC3/TareaEC3/TareaEC3.cpp
@@ -0,0 +1,12 @@
+﻿// TareaEC3.cpp: define el punto de entrada de la aplicación.
+//
+
+#include "TareaEC3.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
